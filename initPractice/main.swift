6212f4bb4f8559f7d490045f06b7ede2f6488eaf
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2022-10-29 19:39:51 +0900

    Perfect

--- initPractice/main.swift
+++ initPractice/main.swift
@@ -1,11 +1,25 @@
-//
-//  main.swift
-//  initPractice
-//
-//  Created by 鈴木彰悟 on 2022/10/29.
-//
+//init 構造体を初期化するためのコードを記述
+//selfキーワードはこの構造体設計図から作成される最終的なオブジエクトを指す
+struct form {
+    //Property  //プロパティー
+    let name: String
+    var animals: [String: Int]
+    var building: [String: Int]
+    
+    init(name: String, animals: [String: Int], building: [String: Int]) {
+       
+        self.name = name
+        self.animals = animals
+        self.building = building
+        
+    }
+    
 
-import Foundation
+}
 
-print("Hello, World!")
+var CowForm = form(name: "shoFarm", animals: ["Cow": 40], building: ["Cowhouse": 2])
 
+print(CowForm.animals)
+
+var HorceForm = form(name: "isLand", animals: ["Horse": 50] ,building: ["HorcePark": 3])
+print(HorceForm)
